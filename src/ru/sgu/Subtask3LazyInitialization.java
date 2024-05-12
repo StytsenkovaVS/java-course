244d9b28a2cfbec5ec512a3569efa65455a7e540
StytsenkovaVS <stytsenkovaler@mail.ru>
2024-05-12 23:38:37 +0400

    task3 initial

--- src/ru/sgu/Subtask3LazyInitialization.java
+++ src/ru/sgu/Subtask3LazyInitialization.java
@@ -1,29 +1,50 @@
 package ru.sgu;
 
+import java.sql.*;
+
 public class Subtask3LazyInitialization {
 
-    public static void main(String[] args) {
-        DatabaseConnection connection = DatabaseConnection.getInstance();
-        connection.executeQuery("SELECT * FROM users");
-    }
-}
+    private static Connection connection;
+    private static String url = "jdbc:postgresql://localhost:5432/java-task7";
+    private static String user = "postgres";
+    private static String password = "123";
 
-class DatabaseConnection {
-
-    private static DatabaseConnection instance;
-
-    private DatabaseConnection() {
-
-    }
-
-    public static DatabaseConnection getInstance() {
-        if (instance == null) {
-            instance = new DatabaseConnection();
+    public static Connection getInstanceOfConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        return instance;
+        return connection;
     }
 
-    public void executeQuery(String query) {
-        System.out.println("Executing query: " + query);
+    public static void getInstanceOfCloseConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = Subtask3LazyInitialization.getInstanceOfConnection();
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT employeeName FROM employeesAge WHERE age > 20");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("employeeName"));
+            }
+
+            Subtask3LazyInitialization.getInstanceOfCloseConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
